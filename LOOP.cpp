afc34b664a3add1ad1ae15084b9d33c16c669b61
jangiryogi <yjangir599@gmail.com>
2025-11-12 00:38:11 +0530

    Update LOOP.cpp

--- LOOP.cpp
+++ LOOP.cpp
@@ -1,4 +1,4 @@
-Write a C++ program that takes a score as input and prints "India wins" if the score is greater than 50, otherwise prints "pak wins".
+ Q1 Write a C++ program that takes a score as input and prints "India wins" if the score is greater than 50, otherwise prints "pak wins".
 
   #include <iostream>
 using namespace std;
@@ -16,4 +16,26 @@ int main() {
     }
 
     cout << "pak wins" << endl;
+}
+
+
+Q2 Write a C++ program using if–else to check whether a given number is a multiple of 6. The number should be less than or equal to 650
+
+  #include <iostream>
+using namespace std;
+
+int main() {
+
+    int num;
+    cout << "Enter a number upto 650: ";
+    cin >> num;
+
+    if(num <= 650 && num % 6 == 0) {
+        cout << num << " is a multiple of 6" << endl;
+    }
+    else {
+        cout << num << " false" << endl;
+    }
+
+    return 0;
 }
