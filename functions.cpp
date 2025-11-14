312c51ee8f3fbd70be697d0db4eaa877b7a3fb77
jangiryogi <yjangir599@gmail.com>
2025-11-14 23:33:23 +0530

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,12 @@
+//Write a function to find the square of a number.
+#include <iostream>
+using namespace std;
+
+int square(int x) {
+    return x * x;
+}
+
+int main() {
+    cout << square(6);
+    return 0;
+}
