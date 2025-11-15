fefbb66a3ed194b75de9892a61611ac93a6374eb
jangiryogi <yjangir599@gmail.com>
2025-11-15 22:48:31 +0530

    Create display the area of circle.cpp

---
+++ some function ouestions.cpp/display the area of circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+float circle_area(float radius){
+    float area = 3.14 * radius * radius
+    return area;
+}
+
+int main() {
+    float radius;
+    cin>>radius;
+    float area = circle_area(rac us);
+    cout<<"Area of Circle is: "<<area<<endl;
+}
