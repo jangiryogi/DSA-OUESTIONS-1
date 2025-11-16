d0a6b5f8a68b766e6854ea823bde96e1adbbb2f9
jangiryogi <yjangir599@gmail.com>
2025-11-16 23:51:37 +0530

    Create hollow pyrmid.cpp

---
+++ patterns.cpp/hollow pyrmid.cpp
@@ -0,0 +1,35 @@
+// Write a C++ program to print a hollow pyramid pattern using stars (*) based on the input number of rows.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int k = 0;
+
+        for (int j = 0; j < ((2 * n) - 1); j++) {
+
+            if (j < n - i - 1) {
+                cout << " ";
+            }
+            else if (k < 2 * i + 1) {
+                if (k == 0 || k == 2 * i || (i == n - 1)) {
+                    cout << "*";
+                } 
+                else {
+                    cout << " ";
+                }
+                k++;
+            }
+            else {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
