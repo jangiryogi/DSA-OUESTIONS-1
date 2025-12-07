e0852dd5173889a9f08b02528105d2e60dc19dd4
jangiryogi <yjangir599@gmail.com>
2025-12-07 23:21:09 +0530

    Create array of colours.cpp

---
+++ array.cpp/array of colours.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string colors[3] = {"red", "blue", "green"};
+
+    for(int i = 0; i < 3; i++) {
+        cout << colors[i] << endl;
+    }
+
+    return 0;
+}
