efd4c4d4af02baf664a7d057f439c819508bc933
jangiryogi <yjangir599@gmail.com>
2025-12-08 23:06:02 +0530

    Create Sum of array elements.cpp

---
+++ array.cpp/Sum of array elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {2, 4, 6, 8, 10};
+    int sum = 0;
+
+    for(int i = 0; i < 5; i++){
+        sum += arr[i];
+    }
+
+    cout << "Sum of elements: " << sum << endl;
+    return 0;
+}
