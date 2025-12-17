e021d9e535dc7e9c182e6bd9fded23568feddd7b
jangiryogi <yjangir599@gmail.com>
2025-12-17 23:33:37 +0530

    Create sum of frist N Natural number.cpp

---
+++ while loop .cpp/sum of frist N Natural number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0, i = 1;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (i <= n) {
+        sum = sum + i;
+        i++;
+    }
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
